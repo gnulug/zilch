9f9315e39020fc8334a7c16c3f2676d3d5a6bc50
evan <evan@evanw.org>
2024-03-03 22:59:10 -0600

    use dataclass for context, use global for indent level

--- zilch/cli.py
+++ zilch/cli.py
@@ -5,15 +5,23 @@ import json
 import os
 import platformdirs
 import os
-from dataclasses import asdict
 from typing import Mapping, Any
+from dataclasses import asdict, dataclass
 from rich.table import Table
 from rich.padding import Padding
 
 from .api import NixPackage, NixSource, ZilchProject
 from console import console
 
-SOURCE = None
+SOURCE = "nixpkgs"
+INDENT = 2
+
+@dataclass
+class Context:
+    verbose: bool = None
+    path: str = None
+    project: ZilchProject = None
+    source: NixSource = None
 
 @click.group(no_args_is_help=True)
 @click.option('--verbose', default=True, is_flag=True)
@@ -26,21 +34,23 @@ SOURCE = None
 )
 @click.pass_context
 def cli(ctx: click.Context, verbose: bool, source: bool, path: pathlib.Path) -> None:
-    ctx.ensure_object(dict)
-    ctx.obj["verbose"] = verbose
-    ctx.obj["path"] = path
-    ctx.obj["project"] = ZilchProject.from_path(path)
-    ctx.obj["source"] = ctx.obj["project"].sources[source] if source is not None else None
-    if ctx.obj["verbose"]:
-        print("Using zilch.toml from", ctx.obj["path"])
+    project = ZilchProject.from_path(path)
+    ctx.obj = Context(
+        verbose,
+        path,
+        project,
+        project.sources[source] if source is not None else None
+    )
+    if ctx.obj.verbose:
+        print("Using zilch.toml from", ctx.obj.path)
 
 @cli.command(no_args_is_help=True)  # @cli, not @click!
 @click.argument('terms', nargs=-1)
 @click.pass_obj
-def search(ctx: Mapping[str, Any], terms: list[str]) -> None:
-    sources = ctx["project"].sources if ctx["source"] is None else [ctx["source"]]
-    for name, source in sources.items():
-        console.rule(f"[yellow]{name}")
+def search(ctx: Context, terms: list[str]) -> None:
+    sources = ctx.project.sources.values() if ctx.source is None else [ctx.source]
+    for source in sources:
+        console.rule(f"[yellow]{source.alias}")
         o = subprocess.run(
             ['nix', 'search', source.url, *terms, '--json'],
             capture_output=True,
@@ -50,13 +60,12 @@ def search(ctx: Mapping[str, Any], terms: list[str]) -> None:
             p = NixPackage(k, source, v['version'], v['description'])
             console.print(f"[green]{p.name}[/green] ({p.version})")
             if p.description != '':
-                console.print(Padding.indent(f"{p.description}", 2))
+                console.print(Padding.indent(f"{p.description}", INDENT))
             console.print('')
 
 
 @cli.command(no_args_is_help=True)  # @cli, not @click!
 @click.argument('term')
-@click.option('--source', default=SOURCE)
 @click.option(
     "--any-source/--match-source",
     default=True,
@@ -65,38 +74,39 @@ def search(ctx: Mapping[str, Any], terms: list[str]) -> None:
         " or only those with a matching source"
     ),
 )
-@click.pass_context
-def info(ctx: click.Context, term: str, any_source: bool) -> None:
-    for p in ctx.obj["project"].packages:
-        if p.name == term and (p.source is None or any_source or p.source == ctx.obj["source"]):
+@click.pass_obj
+def info(ctx: Context, term: str, any_source: bool) -> None:
+    for p in ctx.project.packages:
+        if p.name == term and (p.source is None or any_source or p.source == ctx.source):
+            console.print(f"[green]{p.name}[/green]")
             t = Table(show_lines=False, show_header=False, box=None, pad_edge=False)
             t.add_column()
             t.add_column()
-            for k, v in asdict(p).items():
-                t.add_row(k + ':', v)
-                # console.print(f"  {k}: {v}")
-            console.print(f"[green]{p.name}[/green]")
-            console.print(Padding(t, (0, 0, 0, 2)))
+            t.add_row('description:', p.description)
+            t.add_row('version:', p.version)
+            t.add_row('attribute:', p.attribute)
+            t.add_row('source:', p.source.alias)
+            console.print(Padding.indent(t, INDENT))
             break
     else:
-        print('Package not found')
+        console.print(f'No package [bold]{term}[/bold] is installed')
 
 
 @cli.command(no_args_is_help=True)  # @cli, not @click!
 @click.argument('packages', nargs=-1)
-@click.pass_context
+@click.pass_obj
 def install(ctx: click.Context, packages: list[str]) -> None:
     for package in packages:
-        ctx.obj["project"].add_package(
-            NixPackage.from_name(package, ctx.obj["source"])
+        ctx.project.add_package(
+            NixPackage.from_name(package, ctx.source)
         )
-    ctx.obj["project"].sync()
+    ctx.project.sync()
 
 @cli.command(no_args_is_help=True)  # @cli, not @click!
-@click.pass_context
+@click.pass_obj
 def autoremove(ctx: click.Context) -> None:
-    ctx.obj["project"].sync()
-    ctx.obj["project"].autoremove()
+    ctx.project.sync()
+    ctx.project.autoremove()
 
 @cli.command(no_args_is_help=True)  # @cli, not @click!
 @click.option(
@@ -108,21 +118,25 @@ def autoremove(ctx: click.Context) -> None:
     ),
 )
 @click.argument('packages', nargs=-1)
-@click.pass_context
+@click.pass_obj
 def remove(ctx: click.Context, any_source: bool, packages: list[str]) -> None:
     for package in packages:
-        ctx.obj["project"].remove_package(
-            NixPackage.from_name(package, ctx.obj["source"]),
+        ctx.project.remove_package(
+            NixPackage.from_name(package, ctx.source),
             any_source=any_source,
         )
-    ctx.obj["project"].sync()
+    ctx.project.sync()
 
 @cli.command(no_args_is_help=True)  # @cli, not @click!
 @click.argument('cmd', nargs=-1)
-@click.pass_context
+@click.pass_obj
 def shell(ctx: click.Context, cmd: list[str]) -> None:
     if not cmd:
         cmd = [os.environ.get("SHELL", "/bin/bash")]
-    ctx.obj["project"].sync()
-    env_vars = ctx.obj["project"].get_env_vars()
+    ctx.project.sync()
+    env_vars = ctx.project.get_env_vars()
     os.execvpe(cmd[0], cmd, {**os.environ, **env_vars})
+
+
+if __name__ == '__main__':
+    cli()
